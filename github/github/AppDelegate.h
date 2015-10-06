8d8432852bbb0269d83469ae2ae76c1466fc0049
lanou3g <lanou3g@lanou3gdeMac-mini-9.local>
2015-10-06 14:47:47 +0800

    no message

---
+++ github/github/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  github
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright © 2015年 冯钰书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/github/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  github
+//
+//  Created by lanou3g on 15/10/6.
+//  Copyright © 2015年 冯钰书. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
